90fea1184847b55d817596f489eb134e29537c95
Diego <diego.giglioli@gmail.com>
2022-08-07 01:02:24 -0300

    feat: some includes from scratch

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import DefaultLayout from '@/components/Layouts/DefaultLayout'
+import Main from '@/components/pages/Main'
+
+export default function Home() {
+  return (
+    <DefaultLayout>
+      <Main />
+    </DefaultLayout>
+  )
+}
